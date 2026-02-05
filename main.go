3fc965cd87a4f719f61744c2ec6161c2681dc2ef
Corey Scheideman <corscheid@gmail.com>
2026-02-04 22:47:11 -0600

    Update version

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ var config Config
 
 // global configured instance of Version
 var version = Version{
-	APIVersion:  "1.6.2",
+	APIVersion:  "1.7.0",
 	FwewVersion: fmt.Sprintf("%d.%d.%d", fwew.Version.Major, fwew.Version.Minor, fwew.Version.Patch),
 	DictBuild:   fwew.Version.DictBuild,
 }
